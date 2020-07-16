5fb56fab7e43df222a709eb4fee45429db47b234
j-vent <justine.ventura14@gmail.com>
2020-07-16 15:54:28 +0000

    update text in same page, remove redirect

--- portfolio/webapp/src/pages/CommentForm.js
+++ portfolio/webapp/src/pages/CommentForm.js
@@ -4,24 +4,31 @@ import { Redirect } from 'react-router-dom';
 class CommentForm extends React.Component{
   constructor() {
     super()
-    this.state = {redirect:false}
+    this.state = {messageToUser: "please enter a comment", comment:""}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  handleChange(event){
+      this.setState({comment:event.target.value});
+  }
+
   handleSubmit(event){
-    this.setState({redirect:true});
     event.preventDefault();
+    this.setState({messageToUser:"thanks for posting!"});
+    alert("submit following comment:" + this.state.comment);
+    var params = document.getElementById("textbox");
+    fetch('/data', {method: 'POST', body: params})
+    .then(this.handleRedirect());
   }
+
   render() {
-    const { redirect } = this.state;
-
-    if (redirect) {
-      return <Redirect to='/justine'/>;
-    }
-
+    
     return(
       <div class="content">
-        <form method="POST" action="/data" id = "commentSection" onSubmit = {this.handleSubmit}>
+        <p>{this.state.messageToUser}</p>
+        <form onSubmit = {this.handleSubmit}>
           <p class="center">Leave Justine a comment:</p>
-            <input class="center" id="textbox" name="text" type="text"/>
+            <input class="center" id="textbox" name="text" value={this.state.comment} type="text" onChange={this.handleChange}/>
             <div class="center">
               <input class="commentbtn btn btn-sm btn-outline-primary" type="submit"/>
             </div>
