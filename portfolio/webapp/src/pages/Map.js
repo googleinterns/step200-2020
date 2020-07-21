7632d326323e774b4a93bf0ced080458e2023984
Zachary Stewart <zstewart@google.com>
2020-07-21 14:58:16 -0400

    fix basic map setup

--- portfolio/webapp/src/pages/Map.js
+++ portfolio/webapp/src/pages/Map.js
@@ -14,36 +14,26 @@ const center = {
   lng: -74.0060
 };
  
-export default function Map() {
-  const [map, setMap] = React.useState(null)
- 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
- 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-  console.log(center.lat);
-  console.log(center.lng);
-  return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyAOKxla4a0mvW0OhagpQb4NSRwFndKMNIg">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={8}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  )
+export default class Map extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      map: null,
+    };
+  }
+
+  render() {
+    return (
+      <LoadScript
+        googleMapsApiKey="AIzaSyAOKxla4a0mvW0OhagpQb4NSRwFndKMNIg">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={8}
+          onLoad={map => this.setState({map})}
+          onUnmount={() => this.setState({map: null})} />
+      </LoadScript>
+    )
+  }
 }
- 
-// export default React.memo(Map)
-// export default Map;
