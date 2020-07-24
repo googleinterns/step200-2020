04f29212d43dafb9d93b68c45c296d690007c4b6
Rena Upadhyay <renaupadhyay2022@u.northwestern.edu>
2020-07-24 12:34:50 +0000

    alphabatize imports

--- byway/src/main/java/com/google/sps/servlets/DestinationsServlet.java
+++ byway/src/main/java/com/google/sps/servlets/DestinationsServlet.java
@@ -1,13 +1,13 @@
 package com.google.sps.servlets;
 
-
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/destinations")
