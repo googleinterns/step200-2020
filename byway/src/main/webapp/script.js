6ce31a1815c776ba16a165c465435b2f968be324
j-vent <justine.ventura14@gmail.com>
2020-07-27 16:00:46 +0000

    remove map.js for now

--- byway/src/main/webapp/script.js
+++ byway/src/main/webapp/script.js
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/*
+import {Test} from './map.js';
+let obj = new Test();
+obj.test(); 
+*/
+
 function initMap() {
   var directionsService = new google.maps.DirectionsService();
   var directionsRenderer = new google.maps.DirectionsRenderer();
