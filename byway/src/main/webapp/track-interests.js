423546e1903765cfaae0b7d485bf4393147e6d99
leo <leo.galvez2452@gmail.com>
2020-07-27 17:06:33 +0000

    Address lint issues

--- byway/src/main/webapp/track-interests.js
+++ byway/src/main/webapp/track-interests.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+document.addEventListener('DOMContentLoaded', loadButtons());
+
 let selected = [];
 
 /** 
