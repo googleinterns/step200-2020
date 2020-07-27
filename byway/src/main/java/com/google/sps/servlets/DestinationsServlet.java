ac8aaf6d1c7144fd4921f143f9566e112db23226
Rena Upadhyay <renaupadhyay2022@u.northwestern.edu>
2020-07-27 14:21:49 +0000

    implement datastore

--- byway/src/main/java/com/google/sps/servlets/DestinationsServlet.java
+++ byway/src/main/java/com/google/sps/servlets/DestinationsServlet.java
@@ -1,5 +1,11 @@
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
+
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,30 +20,53 @@ import javax.servlet.http.HttpServletResponse;
 public class DestinationsServlet extends HttpServlet {
 
     private final static class UserLocations{ 
-      private String start = "";
-      private ArrayList<String> destinations =  new ArrayList<String>();
-    }
+      private String start;
+      private ArrayList<String> destinations;
 
-  private final UserLocations places = new UserLocations();
-  private final Gson gson = new Gson();
+        public UserLocations(String start, ArrayList destinations) {
+            this.start = start;
+            this.destinations = destinations;
+        }
+    };
+
+private final UserLocations places = new UserLocations("", new ArrayList<String>());
+private final Gson gson = new Gson();
+DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  Entity destinationsEntity = new Entity("UserDestinations");
+ // Key entityKey = destinationsEntity.getKey());
+
+@Override
+public void init() {
+  destinationsEntity.setProperty("start", "");
+  destinationsEntity.setProperty("destinations", new ArrayList<String>());
+  datastore.put(destinationsEntity);
+}
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String userCurrentandDestinations = gson.toJson(places);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    //Entity entity = datastore.get(entityKey);
+    String start = (String) destinationsEntity.getProperty("start");
+    ArrayList<String> destinations = (ArrayList) destinationsEntity.getProperty("destinations");
+    UserLocations userLocations = new UserLocations(start, destinations);    
     response.setContentType("application/json;");
-    response.getWriter().println(userCurrentandDestinations);
+    response.getWriter().println(gson.toJson(userLocations));
   }
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String current= request.getParameter("start-location");
-    String place = request.getParameter("destinations"); 
-    places.start = current;
-    places.destinations.add(place);
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    String startInput = request.getParameter("start-location");
+    String destination = request.getParameter("destinations"); 
 
-    String userCurrentandDestinations = gson.toJson(places);
-    response.setContentType("application/json;");
-    response.getWriter().println(userCurrentandDestinations);
+    places.start = startInput;
+    places.destinations.add(destination);
+
+    //Entity entity = datastore.get(entityKey);
+    destinationsEntity.setProperty("start", places.start);
+    destinationsEntity.setProperty("destinations", places.destinations);
+    datastore.put(destinationsEntity);
+
     response.sendRedirect("/destinations.html");
   }
 
