d859e56a6496b733d4a2342aae1bef7feda8a8bf
leo <leo.galvez2452@gmail.com>
2020-08-04 21:01:58 +0000

    Add textSearch to find recommendations that fit the interests provided. Temporarily limit results

--- byway/src/main/webapp/recommendations.js
+++ byway/src/main/webapp/recommendations.js
@@ -23,9 +23,12 @@ if(document.readyState === 'loading') {
 let map;
 let placesService;
 let randomLocation;
+let RADIUS = 1000; // Measured in meters
+let interests = ["bank", "groceries", "park"]
 
 /**
- * Initializes the webpage with a map and random location
+ * Initializes the webpage with a map, random location
+ * and recommendations based on user interest.
  */
 function initialize() {
   randomLocation = new google.maps.LatLng(33.4806, -112.1864);
@@ -33,5 +36,48 @@ function initialize() {
       center: randomLocation,
       zoom: 15
   });
-  //placesService = new google.maps.places.PlacesService(map);
+  placesService = new google.maps.places.PlacesService(map);
+  loadRecommendations();
+}
+
+/**
+ * Go through a user's interests and find places
+ * fitting the interests with the textSearch method.
+ */
+function loadRecommendations() {
+  interests.forEach(placeType => {
+      console.log(placeType);
+      let request = {
+        location: randomLocation,
+        radius: RADIUS,
+        query: placeType
+      };
+      placesService.textSearch(request, addRecommendations);
+  });
+}
+
+/**
+ * Checks the response from PlacesService and creates markers
+ * on the locations found from the request. Temporarily limit
+ * the amount of markers added.
+ * TODO: Load all results, but have a list that limits the results
+ * shown rather than limiting the results being loaded.
+ * @param {PlaceResults[] results} places found with PlaceResult type.
+ * @param {PlacesServiceStatus status} status of PlacesService request.
+ */
+function addRecommendations(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    let maxRecommendations = 3;
+    let numRecommendations = 0;
+    for (let i = 0; i < results.length; i++) {
+      console.log(results[i].formatted_address);
+      numRecommendations++;
+      if(numRecommendations == maxRecommendations) {
+        break;
+      }
+    }
+  } else {
+    alert("Status: " + status +
+          "\nOur services are currently down. Oops!");
+  }
 }
