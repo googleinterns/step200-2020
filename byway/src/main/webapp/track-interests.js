77b1c83e26af2b9d1b80405094ad2a810aaa184f
leo <leo.galvez2452@gmail.com>
2020-08-17 20:11:07 +0000

    Fix lints

--- byway/src/main/webapp/track-interests.js
+++ byway/src/main/webapp/track-interests.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* global getTripKeyFromUrl, configureTripKeyForNextPage */
+/* global configureTripKeyForNextPage, getTripKeyFromUrl, setProgressBar*/
 
 let interestsChosen = new Set();
 let tripKey;
