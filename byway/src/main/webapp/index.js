1e9e4831594211b91479565593cb73ecfa755552
Rena Upadhyay <renaupadhyay2022@u.northwestern.edu>
2020-08-17 20:00:20 +0000

    address comments

--- byway/src/main/webapp/index.js
+++ byway/src/main/webapp/index.js
@@ -1,21 +1,23 @@
-window.onload = function(){ 
+if (document.readyState === 'loading') {  // Loading hasn't finished yet
+  document.addEventListener('DOMContentLoaded', initializeLoginPage)
+} 
+else{
+  initializeLoginPage();
+}
+
+function initializeLoginPage(){ 
   fetch("/api/login").then(response => response.json()).then((login) =>{
-  if (login.isLoggedIn) {
     let loginLink = document.createElement("a");
     loginLink.id = "login-button";
-    loginLink.href = "/home.html";
-    loginLink.innerText = "GO TO MY TRIPS";
+    if (login.isLoggedIn) {
+      loginLink.href = "/home.html";
+      loginLink.innerText = "GO TO MY TRIPS";
+    }
+    else{
+      loginLink.href = login.url;
+      loginLink.innerText = "LOGIN";
+    }
     let container = document.getElementById("login-link");
     container.append(loginLink);
-  }
-  else{
-    let loginLink = document.createElement("a");
-    loginLink.id = "login-button";
-    loginLink.href = login.url;
-    loginLink.innerText = "LOGIN";
-    let container = document.getElementById("login-link");
-    container.href = login.url;
-    container.append(loginLink);
-  }
   });
 }
