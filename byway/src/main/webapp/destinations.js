26335ff0f557bc8925ceb91b9b78d4c08a96635c
Rena Upadhyay <renaupadhyay2022@u.northwestern.edu>
2020-08-17 19:22:55 +0000

    fix TODO and buildParams function

--- byway/src/main/webapp/destinations.js
+++ byway/src/main/webapp/destinations.js
@@ -193,7 +193,7 @@ function updateStartDestination(locationData){
 */
 function fetchDestinations(){
     
-  return fetch(buildUrlWithParams("/api/destinations", tripKey)).then(response => response.json());
+  return fetch(buildUrlWithParams("/api/destinations", {tripKey})).then(response => response.json());
 }
 
 /** 
@@ -220,12 +220,12 @@ function getCurrentAddress(){
 window.onload = function(){
   let nextButton = document.getElementById('next-button');
   nextButton.addEventListener('click', () => {
-      nextButton.href = buildUrlWithParams("/interests.html", tripKey);
+      nextButton.href = buildUrlWithParams("/interests.html", {tripKey});
   });
   document.getElementById('user-input-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new  FormData(document.getElementById("user-input-form"));
-    fetch(buildUrlWithParams("/api/destinations", tripKey), {method: 'POST', body:formData}).then((response)=>
+    fetch(buildUrlWithParams("/api/destinations", {tripKey}), {method: 'POST', body:formData}).then((response)=>
         response.json()).then(locationData => {
         updateLocations(locationData);
         updateStartDestination(locationData);
@@ -242,8 +242,8 @@ else{
 }
 
 /** Adds a tripId parameter to a URL string. Assumes the URL has no existing parameters. */
-function buildUrlWithParams(baseUrl, tripKey) {
-  return baseUrl + "?" + new URLSearchParams({"tripKey": tripKey}).toString();
+function buildUrlWithParams(baseUrl, params) {
+  return baseUrl + "?" + new URLSearchParams(params).toString();
 }
 
     
--- byway/src/main/webapp/home.js
+++ byway/src/main/webapp/home.js
@@ -6,7 +6,7 @@ else{
 }
 
 function initializeHomePage(){
-  // TODO: fetch /api/gettrips and display the users past trips
+  // TODO(renau06): fetch /api/gettrips and display the users past trips
   document.getElementById('create-trip').addEventListener('click', () => {
     fetch('/api/createtrip', {method: 'POST'}).then((response) => response.json()).then((trip) =>{
       window.location.href = '/destinations.html?tripKey=' + trip.keyString;  
