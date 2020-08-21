c05a64447801c6fc2a5210fb695955adf8df07eb
j-vent <justine.ventura14@gmail.com>
2020-08-21 15:48:23 +0000

    fix Java format error

--- byway/src/main/java/com/google/sps/servlets/StopsServlet.java
+++ byway/src/main/java/com/google/sps/servlets/StopsServlet.java
@@ -11,12 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+
 package com.google.sps.servlets;
- 
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.common.flogger.FluentLogger;
@@ -24,20 +23,19 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.sps.data.Trip;
 import java.io.IOException;
-import java.lang.NullPointerException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
 /** Servlet that handles data for stops */
 @WebServlet("/api/stop")
 public final class StopsServlet extends HttpServlet {
   private static final Type ARRAYLIST_STRING = new TypeToken<ArrayList<String>>() {}.getType();
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
- 
+
   private final Gson gson = new Gson();
   private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
   // TODO: get key from query string params
@@ -47,29 +45,29 @@ public final class StopsServlet extends HttpServlet {
   /* Passes saved route to be shown in the schedule panel */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
- 
+
     try {
       // trip = Trip.fromEntity(datastore.get(key));
       trip = Trip.getTrip(datastore, (String) request.getParameter("tripKey"));
- 
+
     } catch (NullPointerException e) {
       logger.atInfo().withCause(e).log(
           "Could not retrieve Entity for Trip with key %s while trying to get the stops", key);
       response.setStatus(HttpServletResponse.SC_NOT_FOUND);
       return;
     }
- 
+
     response.setContentType("application/json");
     response.getWriter().println(gson.toJson(trip));
   }
- 
+
   /* Modifies the route array of Trip entity in datastore */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ArrayList<String> stops = gson.fromJson(request.getReader(), ARRAYLIST_STRING);
     try {
       trip = Trip.getTrip(datastore, request.getParameter("tripKey"));
- 
+
     } catch (NullPointerException e) {
       logger.atInfo().withCause(e).log(
           "Could not retrieve Entity for Trip with key %s while trying to update the stops", key);
