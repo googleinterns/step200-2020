a9943c6db4a49e92c37d1cf94b421451e24e192b
j-vent <justine.ventura14@gmail.com>
2020-08-24 23:47:29 +0000

    fix js syntax

--- byway/src/main/webapp/routepage.js
+++ byway/src/main/webapp/routepage.js
@@ -308,7 +308,9 @@ function generateRouteLink(){
 
 
 function sendEmail(){
-  fetch('/api/email').then(response => response.json()).then(email =>{ 
+  fetch('/api/email')
+  .then(response => response.json())
+  .then(email =>{ 
     let emailLink = "mailto:" + email + "?subject=Your%20Roadtrip%20Plan&body=Thank%20you%20for%20using%20Byway!%20" +
       "Your%20destinations%20are%20listed%20below.%20Click%20the%20link%20to%20see%20your%20route%20in%20Google%20Maps.%0D%0A" 
       + encodeURIComponent(generateRouteLink());
