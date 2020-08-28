8576a681e4b63abca2183ff193d8ccfa608614b6
j-vent <justine.ventura14@gmail.com>
2020-08-28 00:12:57 +0000

    remove unused global vars

--- byway/src/main/webapp/js/recommendations.js
+++ byway/src/main/webapp/js/recommendations.js
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/* global destinations, directionsRenderer, directionsService, end, google,
-    interests, map, orderWaypoints, placesService, renderRecsList, route, start, 
+directionsRender
+/* global destinations, google,
+    interests, map, placesService, renderRecsList, start, 
     getRouteForTrip, updatePageInfo*/
 /* exported calcMainRoute, recs */
 
